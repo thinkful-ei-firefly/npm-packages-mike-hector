a3bd38d4dc4023bcc667cb40ff53f09034ad9dec
m-cal <iengineersoftware@gmail.com>
2019-07-24 11:20:07 -0400

    did some stuff

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+import api from './api.js';
+import shoppingList from './shopping-list.js';
+import store from './store.js';
+/* global $*/
+
+$(document).ready(function() {
+  shoppingList.bindEventListeners();
+
+  // On initial load, fetch Shopping Items and render
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    })
+    .catch(err => console.log(err.message));
+});
+
